337313251abee0420197020c56ca5e7f0fabc5b6
Chris A Upshaw <chrisaupshaw@gmail.com>
2013-05-29 14:00:44 -0700

    Changed board a lot.
    Now uses IOArrays.

--- src/Board.hs
+++ src/Board.hs
@@ -1,70 +1,129 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Board where
+{-# LANGUAGE GeneralizedNewtypeDeriving, Rank2Types #-}
+module Board
+  ( PieceSize (..), Player (..), Column (..), Pos, BoardProper, ReserveArray, Board (..)
+  , BoardM (), newBoard, readBoardProper, writeBoardProper, readReserveArray, writeReserveArray
+  , fullBounds, fullRange, saveBoard, mainBoard
+  , runBoardMOnNew
+  ) where
 import System.Exit
 
 import Data.Array.IO
+import Data.Array
 
 import Control.Monad
 import Control.Monad.Reader
-import Control.Monad.IO.Class
+import Control.Monad.IO
+import Control.Monad.Maybe
 
 data PieceSize = S | MS | ML | L
-    deriving (Eq, Ord, Read, Show, Ix, Enum)
+    deriving (Eq, Ord, Read, Show, Ix, Enum, Bounded)
 
 data Player = Black | White
+    deriving (Eq, Ord, Read, Show, Ix, Enum, Bounded)
 
 data Column = C0 | C1 | C2 | C3
-    deriving (Eq, Ord, Enum, Read, Show, Ix)
+    deriving (Eq, Ord, Enum, Read, Show, Ix, Bounded)
 data Row = R0 | R1 | R2 | R3
-    deriving (Eq, Ord, Enum, Read, Show, Ix)
+    deriving (Eq, Ord, Enum, Read, Show, Ix, Bounded)
 
 type Pos = (Column, Row)
 
 type BoardProper = IOArray (Pos, PieceSize) (Maybe Player)
 
-data ReservesAmount = RA1 | RA2 | RA3
-    deriving (Eq, Ord, Read, Show)
+data PileID = P0 | P1 | P2
+    deriving (Eq, Ord, Enum, Read, Show, Ix, Bounded)
 
-type ReserveArray = IOArray (Player, Pile) (Maybe PieceSize)
+type ReserveArray = IOArray (Player, PileID) (Maybe PieceSize)
 
-data Board = Board
+type Board
     {boardProper :: BoardProper, reserveArray :: ReserveArray}
 
-newtype BoardM a = BoardM (ReaderT Board IO a)
+newtype BoardM a = BoardM (ReaderT (Board) (IO) a)
     deriving (Monad, Functor)
 
-newBoard :: IO Board
-newBoard = Board `liftM2` (makeMaxArray Nothing) (makeMaxArray (Just L)) 
+mkBoard :: IO (BoardProper) -> IO (ReserveArray) -> IO (Board)
+mkBoard = liftM2 Board
+
+newBoard :: IO (Board)
+newBoard = mkBoard (makeMaxArray Nothing) (makeMaxArray (Just L)) 
    where
-   makeMaxArray :: (Ix x, Bounded x) => e -> IOArray x e
-   makeMaxArray = newArray (minBound, maxBound)
+   makeMaxArray :: (Ix x, Bounded x) => e -> IO (IOArray x e)
+   makeMaxArray fillValue = newArray fullBounds fillValue
 
-runBoardM (BoardM m) board = runReaderT board m
+runBoardMOnNew :: BoardM a -> 
 
-readProperBoard x = 
+runBoardM :: BoardM a -> Board -> IO a
+runBoardM (BoardM m) arr = runReaderT m arr
+
+cloneArray = thaw . unsafeFreeze
+
+runBoardMOnCopy :: BoardM a -> Board -> IO a
+runBoardMOnCopy m arr =
+  do
+  boardProperClone <- clone =<< boardProper arr
+  reservesClone <- clone =<< reserveArray arr
+  runBoardM m (Board boardProperClone reservesClone)
+
+readArrayBM :: (Ix ix) => (Board -> IOArray ix e) -> ix -> BoardM e
+readArrayBM getArray x = 
     do
-    arr <- BoardM (asks boardProper)
-    BoardM (liftIO (readArray arr x))
+    arr <- BoardM (asks getArray)
+    BoardM (lift (readArray arr x))
 
-writeProperBoard x p =
+writeArrayBM :: (Ix ix) => (Board -> IOArray ix e) -> ix -> e -> BoardM ()
+writeArrayBM getArray x p =
     do
-    arr <- asks boardProper
-    BoardM (liftIO (writeArray arr x p))
+    arr <- BoardM (asks getArray)
+    BoardM (lift (writeArray arr x p))
 
-saveArray arr =
-   do
+readBoardProper :: (Pos, PieceSize) -> BoardM (Maybe Player)
+readBoardProper = readArrayBM boardProper
+writeBoardProper :: (Pos, PieceSize) -> Maybe Player -> BoardM ()
+writeBoardProper = writeArrayBM boardProper
+readReserveArray :: (Player, PileID) -> BoardM (Maybe PieceSize)
+readReserveArray = readArrayBM reserveArray
+writeReserveArray :: (Player, PileID) -> Maybe PieceSize -> BoardM ()
+writeReserveArray = writeArrayBM reserveArray
 
+fullBounds :: (Ix ix, Bounded ix) => (ix, ix)
+fullBounds = (minBound, maxBound)
 
-saveProperBoard =
+fullRange :: (Ix ix, Bounded ix) => [ix]
+fullRange = range fullBounds
+
+saveArray :: (Ix ix, Bounded ix) => (Board -> IOArray ix e) -> (e -> Char) -> BoardM String
+saveArray getArray toChar =
     do
-    arr <- asks boardProper
-    b <- getBounds arr
-    forM (range b) (toChar `liftM' readArray arr)
+    arr <- BoardM (asks getArray)
+    BoardM . lift $
+      forM fullRange 
+      (\ ix -> toChar `liftM` readArray arr ix)
+
+--a zip that fails if the two lists are differnt in length
+exactZip :: [a] -> [b] -> Maybe [(a,b)]
+exactZip [] [] = return []
+exactZip (x:xs) (y:ys) = ((x,y) :) `fmap` exactZip xs ys
+
+saveProperBoard :: BoardM String
+saveProperBoard = saveArray boardProper toChar
     where
     toChar :: Maybe Player -> Char
     toChar Nothing = '-'
-    toChar (Just White) = 'O'
-    toChar (Just Black = 'X'
+    toChar (Just White) = 'W'
+    toChar (Just Black) = 'B'
+
+saveReserveArray :: BoardM String
+saveReserveArray = saveArray reserveArray toChar
+  where
+  toChar :: Maybe PieceSize -> Char
+  toChar Nothing = '-'
+  toChar (Just S) = 'S'
+  toChar (Just MS) = 'm'
+  toChar (Just ML) = 'M'
+  toChar (Just L) = 'L'
     
+saveBoard :: BoardM String
+saveBoard = liftM2 (++) saveProperBoard saveReserveArray 
+
 mainBoard :: IO ExitCode
 mainBoard = return ExitSuccess
